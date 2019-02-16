3e458db48553ec5494140a4f4a86fc514f787e9a
Rex.Song <westerncampus@gmail.com>
2019-02-16 17:25:48 +0800

    Add project files.

---
+++ UWPSolutionBDD/Class1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenQA.Selenium.Appium.Windows;
+using OpenQA.Selenium.Remote;
+
+namespace UWPSolutionBDD
+{
+    public class Class1
+    {
+        public void Test()
+        {
+
+        }
+    }
+}
---
+++ UnitTestProject2/UnitTest1.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium.Appium.Windows;
+using OpenQA.Selenium.Remote;
+
+namespace UnitTestProject2
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        // Note: append /wd/hub to the URL if you're directing the test at Appium
+        private const string WindowsApplicationDriverUrl = "http://127.0.0.1:4723";
+        private const string CalculatorAppId = @"ea0f88c8-a076-44fd-bbf9-db20af1c9913_kad5tadq656qm!App";
+        protected static WindowsDriver<WindowsElement> session;
+
+        [TestInitialize]
+        public void Setup()
+        {
+            // Launch Calculator application if it is not yet launched
+            if (session == null)
+            {
+                // Create a new session to bring up an instance of the Calculator application
+                // Note: Multiple calculator windows (instances) share the same process Id
+                DesiredCapabilities appCapabilities = new DesiredCapabilities();
+                appCapabilities.SetCapability("app", CalculatorAppId);
+                appCapabilities.SetCapability("deviceName", "WindowsPC");
+                session = new WindowsDriver<WindowsElement>(new Uri(WindowsApplicationDriverUrl), appCapabilities);
+                Assert.IsNotNull(session);
+
+                // Set implicit timeout to 1.5 seconds to make element search to retry every 500 ms for at most three times
+                session.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(1.5);
+            }
+        }
+
+        [TestCleanup]
+        public void TearDown()
+        {
+            // Close the application and delete the session
+            if (session != null)
+            {
+                session.Quit();
+                session = null;
+            }
+        }
+
+        [TestMethod]
+        public void TestMethod1()
+        {
+            var bt = session.FindElementByName("Button");
+            bt.Click();
+        }
+
+        [TestMethod]
+        public void TestMethod2()
+        {
+            var view = session.FindElementByName("登录");
+            view.Click();
+        }
+    }
+}
